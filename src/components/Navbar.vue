faf5d0cc7241645b781603c6f3b10a2fc68a4dbc
elise-lcdb <edebeauvais@hotmail.com>
2019-01-31 23:11:38 +0100

    Navbar modified and Live page added by following wireframes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,70 @@
 <template>
-  <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-  <a class="navbar-brand" href="#"><img src="../assets/yesport-logo-white.png" style="height:50px"></a>
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link</a>
-    </li>
-  </ul>
-</nav>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <div class="container">
+      <router-link class="navbar-brand" to="/live">
+        <img src="../assets/yesport-logo-white.png" />
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#collapsibleNavbar"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <form class="form-inline" action="/action_page.php">
+            <input
+              class="form-control mr-sm-2"
+              type="text"
+              placeholder="Search"
+            />
+          </form>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">
+              Home
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">
+              Paris Sportif
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/live">
+              Live
+            </router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">
+              <button type="button" class="btn btn-secondary">
+                Se Connecter
+              </button>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">
+              <button type="button" class="btn btn-secondary">
+                S'inscrire
+              </button>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-  name: "Navbar",
+  name: "Navbar"
 };
 </script>
 <style scoped>
-
+.navbar-brand img {
+  height: 40px;
+}
 </style>
--- src/views/Live.vue
+++ src/views/Live.vue
@@ -1,5 +1,90 @@
 <template>
-  <div class="about">
-    <h1>This is live page</h1>
-  </div>
+  <section class="section about">
+    <div class="container">
+      <br />
+      <div class="row">
+        <div class="col-md-9">
+          <div class="row">
+            <div class="col-md-12 live_video">
+              <!-- Filler Video -->
+              <iframe
+                src="https://www.youtube.com/embed/E4pyzeTvFC0"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </div>
+          <br />
+          <div class="row">
+            <div class="col-md-12">
+              <div class="live_infos">
+                Infos sur le live (titre, jeux, joueurs)
+              </div>
+            </div>
+          </div>
+          <br />
+          <div class="row">
+            <div class="col-md-6">
+              <div class="live_prono">Pronostics</div>
+            </div>
+            <br />
+            <div class="col-md-6">
+              <div class="live_quiz">Quizz</div>
+            </div>
+            <br />
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="live_chat">Chat</div>
+            </div>
+          </div>
+          <br />
+          <div class="row">
+            <div class="col-md-12">
+              <div class="live_vocal">Plateforme vocale</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+<style scoped>
+.live_chat,
+.live_vocal,
+.live_infos,
+.live_quiz,
+.live_prono {
+  background: gray;
+}
+.live_video iframe {
+  min-height: 100%;
+  width: 100%;
+  height: 70vh;
+}
+.live_infos {
+  height: 10vh;
+  min-height: 50px;
+}
+.live_chat {
+  height: 55vh;
+  min-height: 150px;
+}
+.live_vocal {
+  height: 55vh;
+  min-height: 150px;
+}
+.live_quiz {
+  min-height: 100px;
+  height: 25vh;
+  margin-bottom: 1em;
+}
+.live_prono {
+  min-height: 100px;
+  height: 25vh;
+  margin-bottom: 1em;
+}
+</style>
