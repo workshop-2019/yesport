bf0b29e0f146c682be32481ae8cc28cb2eade054
elise-lcdb <edebeauvais@hotmail.com>
2019-01-31 14:20:24 +0100

    Bootstrap added to app.vue

--- src/App.vue
+++ src/App.vue
@@ -13,3 +13,6 @@ export default {
   components: { Navbar }
 };
 </script>
+<style lang="scss">
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,15 @@
 <template>
-  <nav class="navbar">
-   <!--  <router-link to="/">Home</router-link> -->
-  </nav>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+  <a class="navbar-brand" href="#"><img src="../assets/yesport-logo-white.png" style="height:50px"></a>
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+  </ul>
+</nav>
 </template>
 
 <script>
@@ -9,3 +17,6 @@ export default {
   name: "Navbar",
 };
 </script>
+<style scoped>
+
+</style>
