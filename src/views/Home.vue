8e4b948f6507e77451a2d0db4a300449b4d9d6e0
GabrielleLng <gabrielle.leung@ynov.com>
2019-02-02 02:22:02 +0100

    Home page modified - test with content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,27 +7,53 @@
                     <!-- carousel -->
                     <div class="row">
                         <div class="col-md-12">
-                            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                                <div class="carousel-inner">
-                                    <div class="carousel-item active">
-                                    <img class="d-block w-100" src="https://via.placeholder.com/1110x400" alt="First slide">
-                                    </div>
-                                    <div class="carousel-item">
-                                    <img class="d-block w-100" src="https://via.placeholder.com/1110x400" alt="Second slide">
-                                    </div>
-                                    <div class="carousel-item">
-                                    <img class="d-block w-100" src="https://via.placeholder.com/1110x400" alt="Third slide">
-                                    </div>
-                                </div>
-                                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                    <span class="sr-only">Previous</span>
-                                </a>
-                                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                    <span class="sr-only">Next</span>
-                                </a>
-                            </div>    
+                            <div class="carousel slide" id="carousel-956080">
+				<ol class="carousel-indicators">
+					<li data-slide-to="0" data-target="#carousel-956080" class="active">
+					</li>
+					<li data-slide-to="1" data-target="#carousel-956080">
+					</li>
+					<li data-slide-to="2" data-target="#carousel-956080">
+					</li>
+				</ol>
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<img class="d-block w-100" alt="Carousel Bootstrap First" src="http://misc.team-aaa.com/perso_skullgun/CSGO/CSGO_Headers_w700_h247.jpg">
+						<div class="carousel-caption">
+							<h4>
+								First Thumbnail label
+							</h4>
+							<p>
+								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							</p>
+						</div>
+					</div>
+					<div class="carousel-item">
+						<img class="d-block w-100" alt="Carousel Bootstrap Second" src="https://gamingtest.fr/wp-content/uploads/2012/11/counter-strike-global-offensive-592x274.jpg">
+						<div class="carousel-caption">
+							<h4>
+								Second Thumbnail label
+							</h4>
+							<p>
+								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							</p>
+						</div>
+					</div>
+					<div class="carousel-item">
+						<img class="d-block w-100" alt="Carousel Bootstrap Third" src="https://i2.wp.com/www.connectesport.com/alpha/wp-content/uploads/2016/12/csgo.jpg?w=940&ssl=1">
+						<div class="carousel-caption">
+							<h4>
+								Third Thumbnail label
+							</h4>
+							<p>
+								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							</p>
+						</div>
+					</div>
+				</div> <a class="carousel-control-prev" href="#carousel-956080" data-slide="prev"><span class="carousel-control-prev-icon"></span> <span class="sr-only">Previous</span></a> <a class="carousel-control-next" href="#carousel-956080" data-slide="next"><span class="carousel-control-next-icon"></span> <span class="sr-only">Next</span></a>
+			</div>
+                           
+
                         </div>
                     </div>
                     <!-- bloc 1 -->
@@ -35,16 +61,72 @@
                         
                         <div class="col-md-12 ">
                             <br/>
-                            <p>Live par catégories (type de jeu par exemple) :</p>
+                            <div class="row">
+                                <div class="col-md-12 ">
+                                    <div class="home-category">
+                                        <p>Live par catégories (type de jeu par exemple) :</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                               <div class="col-md-4 col-sm-6 col-xs-12">
+                                    <div class="blog-post">
+                                        <div class="post-img">
+                                            <iframe src="https://player.twitch.tv/?channel=vitality" frameborder="0" allowfullscreen="true" scrolling="no" height="196" width="100%"></iframe>
+                                        </div>    
+                                        <div class="blog-details">
+                                            <h4 class="media-heading">Infos sur le live</h4><hr>
+                                            <p>Lorem Ipsum adalah text contoh digun akan didalam industri pence taka types etting 
+                                            adalah text contoh.</p>
+                                            
+                                        </div>
+                                    </div>    
+                                </div>    
+                                <div class="col-md-4 col-sm-6 col-xs-12">
+                                    <div class="blog-post">
+                                        <div class="post-img">
+                                            <iframe src="https://player.twitch.tv/?channel=blastproseries" frameborder="0" allowfullscreen="true" scrolling="no" height="196" width="100%"></iframe>
+                                        </div>
+                                        <div class="blog-details">
+                                            <h4 class="media-heading">Infos sur le live</h4><hr>
+                                            <p>Lorem Ipsum adalah text contoh digun akan didalam industri pence taka types etting 
+                                            adalah text contoh.</p>
+                                            
+                                        </div>
+                                    </div>    
+                                </div>    
+                                <div class="col-md-4 col-sm-6 col-xs-12">
+                                    <div class="blog-post">
+                                        <div class="post-img">
+                                            <iframe src="https://player.twitch.tv/?channel=esl_csgo" frameborder="0" allowfullscreen="true" scrolling="no" height="196" width="100%"></iframe>
+                                            </div>    
+                                        <div class="blog-details">
+                                            <h4 class="media-heading">Infos sur le live</h4><hr>
+                                            <p>Lorem Ipsum adalah text contoh digun akan didalam industri pence taka types etting 
+                                            adalah text contoh.</p>
+                                            
+                                        </div>
+                                    </div>    
+                                </div>    
+                            </div>
+                            <br />
+                            <br />
+                            <div class="row">
+                                <div class="col-md-12 ">
+                                    <div class="home-category">
+                                        <p>Live par catégories (type de jeu par exemple) :</p>
+                                    </div>
+                                </div>
+                            </div>
                             <div class="row">
     
                                <div class="col-md-4 col-sm-6 col-xs-12">
                                     <div class="blog-post">
                                         <div class="post-img">
-                                            <img class="img-responsive" src="../assets/bg-stadium.jpg" alt="image" width="100%">
+                                            <iframe src="https://player.twitch.tv/?channel=syndicate" frameborder="0" allowfullscreen="true" scrolling="no" height="196" width="100%"></iframe>
                                         </div>    
                                         <div class="blog-details">
-                                            <h4 class="media-heading">Infos sur le live</h4>
+                                            <h4 class="media-heading">Infos sur le live</h4><hr>
                                             <p>Lorem Ipsum adalah text contoh digun akan didalam industri pence taka types etting 
                                             adalah text contoh.</p>
                                             
@@ -54,10 +136,10 @@
                                 <div class="col-md-4 col-sm-6 col-xs-12">
                                     <div class="blog-post">
                                         <div class="post-img">
-                                            <img class="img-responsive" src="../assets/bg-stadium.jpg" alt="image" width="100%">
+                                            <iframe src="https://player.twitch.tv/?channel=riotgames" frameborder="0" allowfullscreen="true" scrolling="no" height="196" width="100%"></iframe>
                                         </div>    
                                         <div class="blog-details">
-                                            <h4 class="media-heading">Infos sur le live</h4>
+                                            <h4 class="media-heading">Infos sur le live</h4><hr>
                                             <p>Lorem Ipsum adalah text contoh digun akan didalam industri pence taka types etting 
                                             adalah text contoh.</p>
                                             
@@ -67,73 +149,47 @@
                                 <div class="col-md-4 col-sm-6 col-xs-12">
                                     <div class="blog-post">
                                         <div class="post-img">
-                                            <img class="img-responsive" src="../assets/bg-stadium.jpg" alt="image" width="100%">
+                                            <iframe src="https://player.twitch.tv/?channel=ninja" frameborder="0" allowfullscreen="true" scrolling="no" height="196" width="100%"></iframe>
                                         </div>    
                                         <div class="blog-details">
-                                            <h4 class="media-heading">Infos sur le live</h4>
-                                            <p>Lorem Ipsum adalah text contoh digun akan didalam industri pence taka types etting 
-                                            adalah text contoh.</p>
-                                            
-                                        </div>
-                                    </div>    
-                                </div>    
-                            </div>
-                            <br/>
-                            <p>Live par catégories (type de jeu par exemple) :</p>
-                            <div>
-
-                            </div>
-                            <div class="row">
-    
-                               <div class="col-md-4 col-sm-6 col-xs-12">
-                                    <div class="blog-post">
-                                        <div class="post-img">
-                                            <img class="img-responsive" src="../assets/bg-stadium.jpg" alt="image" width="100%">
-                                        </div>    
-                                        <div class="blog-details">
-                                            <h4 class="media-heading">Infos sur le live</h4>
-                                            <p>Lorem Ipsum adalah text contoh digun akan didalam industri pence taka types etting 
-                                            adalah text contoh.</p>
-                                            
-                                        </div>
-                                    </div>    
-                                </div>    
-                                <div class="col-md-4 col-sm-6 col-xs-12">
-                                    <div class="blog-post">
-                                        <div class="post-img">
-                                            <img class="img-responsive" src="../assets/bg-stadium.jpg" alt="image" width="100%">
-                                        </div>    
-                                        <div class="blog-details">
-                                            <h4 class="media-heading">Infos sur le live</h4>
-                                            <p>Lorem Ipsum adalah text contoh digun akan didalam industri pence taka types etting 
-                                            adalah text contoh.</p>
-                                            
-                                        </div>
-                                    </div>    
-                                </div>    
-                                <div class="col-md-4 col-sm-6 col-xs-12">
-                                    <div class="blog-post">
-                                        <div class="post-img">
-                                            <img class="img-responsive" src="../assets/bg-stadium.jpg" alt="image" width="100%">
-                                        </div>    
-                                        <div class="blog-details">
-                                            <h4 class="media-heading">Infos sur le live</h4>
+                                            <h4 class="media-heading">Infos sur le live</h4><hr>
                                             <p>Lorem Ipsum adalah text contoh digun akan didalam industri pence taka types etting 
                                             adalah text contoh.</p>
                                         </div>
                                     </div>    
                                 </div>    
                             </div>
+                            
                         </div>
                     </div>
                     <br />
                     <!-- footer -->
+                    <footer class="footer">
+                
                     <div class="row">
-                        <div class="col-md-12 home_footer">
-                            Footer
-                        </div>
-                    </div>
+		<div class="col-md-4 footer-content">
+            
+			    <img src="../assets/yesport-logo-white.png" width="200"/> 
+            
+			<address>
+				 <strong>YNOV - YESPORT TEAM</strong><br /> 12 rue Anatole France<br /> 92000 - Nanterre<br /> <abbr title="Phone">Tél:</abbr> +33(0)6 00 00 00 00
+			</address>
+		</div>
+		<div class="col-md-4">
+			<p>
+				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. </small>
+			</p>
+		</div>
+		<div class="col-md-4">
+			<p>
+				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. </small>
+			</p>
+		</div>
+	</div>
+                       
+                    </footer>
                 </div>
+               
             </div>
         </div>
     </div>
@@ -144,28 +200,55 @@ export default {
   name: "Home"
 };
 </script>
+
+
 <style scoped>
+.home{
+    background-image:url("https://static.wixstatic.com/media/41d000_09fb5faf639d7079be4d0f79b4818bbf.png");
+    background-color:#1a1a1a;
+}
 .home-carousel {
   min-height: 100%;
   width: 100%;
   height: 70vh;
 }
+.home-category p{
+    margin: 10px 0;
+    color: rgb(187, 187, 187);
+}
 .home_live, 
 .home_footer {
-    background : gray;
+    background-color: rgba(0,0,0,0.5);
     height: 30vh;
 }
-.item_desc {
-  background: gray;
-}
-
+.item_desc,
 .blog-post {
-    background: gray;
+  background: gray;
+  font-weight:lighter;
+}
+.blog-post{
+    border: 1px solid dimgray;
 }
 .blog-details{
     padding: 20px 20px;
+    background-color: #1a1a1a ;
+    color: white;
 }
 .blog-details h4 {
     text-align: center;
+    font-size: 1.125rem;
 }
+.footer-content{
+    padding: 20px 20px;
+}
+footer{
+    align-items:stretch;
+    color:darkgray;
+    background-color: rgba(0,0,0,0.5);
+   padding: 20px 20px;
+}
+.carousel{
+    padding-top: 60px;
+}
+
 </style>
